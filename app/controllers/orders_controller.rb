88ada1da1d878cfae55c6348b41579af14871529
Steve Kinney <hello@stevekinney.net>
2014-09-14 22:34:45 -0600

    Implement order scoping based on user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
+
   def index
-    @orders = Order.all
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    
+    user ||= User.new
+
+    can :manage, Order, :user_id => user.id
   end
 end
