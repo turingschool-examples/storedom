c00ca0fe872f59df9b851bb021dad5a633b88d5b
Jeff Casimir <jeff@casimircreative.com>
2014-10-28 13:56:12 -0600

    Derp pull out worker implementation

--- app/generators/user_generator.rb
+++ app/generators/user_generator.rb
@@ -1,22 +1,11 @@
 class UserGenerator
-  include Sidekiq::Worker
-
-  def perform(marker)
-    generate_one(marker)
-  end
-
   def generate(quantity)
     quantity.times do |i|
-      UserGenerator.perform_async(i)
+      user = User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email
+        )
+      puts "User #{i}: #{user.name} - #{user.email} created!"
     end
-  end
-
-  def generate_one(marker)
-    Delay.wait
-    user = User.create!(
-      name: Faker::Name.name,
-      email: Faker::Internet.email
-      )
-    puts "User #{marker}: #{user.name} - #{user.email} created!"
   end
 end
