d83f7527886e220581213b71cdfe44abe111421e
Steve Kinney <hello@stevekinney.net>
2014-11-03 12:50:15 -0700

    Add a scope to the order model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    # This will work, but you're better off using a scope.
-    can :manage, Order, Order.includes(:user).where(users: { manager_id: user.id }) do |order|
+    can :manage, Order, Order.managed_by_user(user.id) do |order|
       order.user.manager_id == user.id
     end
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ActiveRecord::Base
   belongs_to :user
 
+  scope :managed_by_user, ->(user) { includes(:user).where(users: { manager_id: user }) }
+
   has_many :order_items
   has_many :items, through: :order_items
 end
