b9372aacbadd90dd1f1a7a5fd6f077d88d897d7d
Steve Kinney <hello@stevekinney.net>
2014-11-03 10:42:47 -0700

    Add a manager_id to the Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915035526) do
+ActiveRecord::Schema.define(version: 20141103173959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20140915035526) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.integer  "manager_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
