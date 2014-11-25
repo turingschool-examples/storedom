1d38e2bcd231c5fa0ffd6cd3086351a589523f96
Jeff Casimir <jeff@casimircreative.com>
2014-11-24 21:06:00 -0700

    Add a button (which submits a POST) and a set of routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :items,    only:   [:index, :show]
   resources :orders,   only:   [:index, :show]
   resources :users,    except: [:edit, :destroy]
+  resources :cart_items
 
   get    '/login',  to: "sessions#new"
   post   '/login',  to: "sessions#create"
