4ad88e1271f12778778bde2733cd25e5619c72bd
Jeff Casimir <jeff@casimircreative.com>
2014-11-24 21:41:29 -0700

    Build up the real functionality of the Cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,16 @@
 class Cart
-  def initialize(data)
+  attr_reader :data
 
+  def initialize(input_data)
+    @data = input_data || Hash.new
   end
 
   def add_item(item)
-
+    data[item.id.to_s] ||= 0
+    data[item.id.to_s] += 1
   end
 
   def count_of(item)
-    2
+    data[item.id.to_s]
   end
 end
