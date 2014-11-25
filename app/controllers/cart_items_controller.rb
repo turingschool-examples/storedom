db3f529dfe20ca1c38cae196cf7700a141a53e28
Jeff Casimir <jeff@casimircreative.com>
2014-11-24 21:20:04 -0700

    Fix the redirect destination

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,6 +4,6 @@ class CartItemsController < ApplicationController
   def create
     item = Item.find(params[:item_id])
     @cart.add_item(item)
-    redirect_to items_path(item)
+    redirect_to item_path(item)
   end
 end
