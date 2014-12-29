150540c7016e49656f573a538c2c2c326fa566b0
worace <horace.d.williams@gmail.com>
2014-12-28 22:05:33 -0600

    clean slate for orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,11 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  load_and_authorize_resource
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 end
