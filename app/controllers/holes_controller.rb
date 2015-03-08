9800557459faadd26716467c28f86e17d1a150fd
Josh Mejia <joshua.mejia@gmail.com>
2015-03-08 13:06:28 -0600

    Add test for hole five for polymorphism

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -19,4 +19,20 @@ class HolesController < ApplicationController
     @orders = Order.largest
     render 'orders'
   end
+
+  def five # par 4
+    # 1. Make the test located in /test/models/image_test.rb pass.
+    #    (Hint: The database will need to be updated and it's polymorphic.)
+    #
+    # 2. If everything is good, you should be rewarded when
+    #    you visit /holes/five
+    @image_1 = Image.where(url: 'https://placekitten.com/g/200/300',
+                           imageable_type: 'User',
+                           imageable_id: 1).first_or_create
+    @image_2 = Image.where(url: 'http://placekitten.com/450/300'
+                           imageable_type: 'Item',
+                           imageable_id: 2).first_or_create
+                          ).first_or_create
+    render 'images'
+  end
 end
---
+++ test/models/image_test.rb
@@ -0,0 +1,21 @@
+require_relative '../test_helper'
+
+class ImageTest < ActiveSupport::TestCase
+  test "it has a polymorphic relationship with users and items" do
+    user = User.create(name: "Richard")
+    item = Item.create(name: "Blunderbuss")
+
+    # This assumes a user has_many images
+    user.images.create(url: "http://placekitten.com/450/300")
+
+    # This assumes an item has_many images
+    item.images.create(url: "https://placekitten.com/g/200/300")
+
+    assert_equal "User", image_1.imageable_type
+    assert_equal user.id, image_1.imageable_id
+
+    assert_equal "Item", image_2.imageable_type
+    assert_equal item.id, image_2.imageable_id
+  end
+end
+
