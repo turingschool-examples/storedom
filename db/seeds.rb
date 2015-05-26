d92fc78845f96a91d4f2d4ada8d17ae43229510b
Josh Mejia <joshua.mejia@gmail.com>
2015-05-26 15:14:14 -0600

    Begin transition toward efficient seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,52 @@
-# User 1 with an order
-user_1  = User.create(name: "Richard", email: "richard@example.com", password: "asdf")
-puts "Created User: #{user_1.name}"
+class Seed
+  def call
+    generate_items
+    generate_default_user
+    generate_users
+    generate_orders
+  end
 
-item_1  = Item.create(name: "Bicycle", description: "This is a description.")
-puts "Created Item: #{item_1.name}"
+  def self.call
+    new.call
+  end
 
-order_1 = Order.create(amount: 10.00, user_id: user_1.id)
-order_1.items << item_1
-puts "Created Order For: #{order_1.user.name}"
+  private
 
-# User 1 with an order
-user_2  = User.create(name: "Ritchie Valens", email: "ritchie@example.com", password: "asdf")
-puts "Created User: #{user_2.name}"
+  def generate_default_user
+    # Use build inside of create blocks to set up associations
+    User.create(name: "Richard", email: "richard@example.com", password: "asdf") do |user|
+      user.orders.build(amount: Faker::Commerce.price) do |order|
+        order.items << Item.order("RANDOM()").limit(1).first
+      end
+    end
+  end
 
-item_2  = Item.create(name: "Guitar", description: "Oh Donna.")
-puts "Created Item: #{item_2.name}"
+  def generate_orders
+    100.times do
+      user  = User.order("RANDOM()").limit(1).first
+      user.orders.create(amount: Faker::Commerce.price) do |order|
+        rand(1..10).times do
+          order.items << Item.order("RANDOM()").limit(1).first
+        end
+      end
+      puts "Created Orders For: #{user.name}"
+    end
+  end
 
-order_2 = user_2.orders.create(amount: 10.00)
-order_2.items << item_2
-puts "Created Order For: #{order_2.user.name}"
+  def generate_users
+    10.times do
+      user  = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: "asdf")
+      puts "Created User: #{user.name}"
+    end
+  end
+
+  def generate_items
+    1000.times do
+      item  = Item.create(name: Faker::Commerce.product_name, description: Faker::Lorem.sentence)
+      puts "Created Item: #{item.name}"
+    end
+  end
+end
+
+Seed.call
 
