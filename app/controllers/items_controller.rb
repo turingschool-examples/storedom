a38255151240877769e651fff0ea5fce31a9b990
novohispano <novohispano@gmail.com>
2015-05-31 23:15:37 -0600

    Prepare app for API class

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,48 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id].to_i)
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to items_path, notice: "The item was created."
+    else
+      flash.now[:notice] = "The item was not created."
+      render :edit
+    end
+  end
+
+  def edit
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(item_params)
+      redirect_to item_path(@item), notice: "The item was updated."
+    else
+      flash.now[:notice] = "The item was not updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+
+    redirect_to items_path, notice: "The item was deleted."
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,46 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: "The user was created."
+    else
+      flash.now[:notice] = "The user was not created."
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user), notice: "The user was updated."
+    else
+      flash.now[:notice] = "The user was not updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    User.destroy(params[:id])
+
+    redirect_to users_path, notice: "The user was deleted."
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
