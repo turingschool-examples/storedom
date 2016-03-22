88bd32172a5dd4716da2ce4694522d7aa821f7a7
Nate Allen <neight.allen@gmail.com>
2016-03-22 10:57:38 -0600

    Created store model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216235605) do
+ActiveRecord::Schema.define(version: 20160322165554) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20131216235605) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "stores", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "stores", ["user_id"], name: "index_stores_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
