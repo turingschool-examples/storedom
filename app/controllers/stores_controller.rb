4d8c3b15f42eaf8e4f407c2259b741f27f31b611
Nate Allen <neight.allen@gmail.com>
2016-03-22 11:06:57 -0600

    Created StoresController

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,4 @@
+class StoresController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'stores#index'
 
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
